8672e5f91d123ec624d00666c46429e71cbe5eb7
Jeremy A. Russell <jeremyarussell@gmail.com>
2019-11-26 07:19:54 -0800

    Initial commit
    First verison of the application. Made this awhile back and wanted to update it and decided why not share it while I do that?

---
+++ Simple Stream Journal/Main.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Simple_Stream_Journal
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.controlOldContentTextBox = new System.Windows.Forms.TextBox();
+            this.controlCurrentContentTextBox = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.controlStampTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // controlOldContentTextBox
+            // 
+            this.controlOldContentTextBox.AcceptsReturn = true;
+            this.controlOldContentTextBox.AcceptsTab = true;
+            this.controlOldContentTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlOldContentTextBox.HideSelection = false;
+            this.controlOldContentTextBox.Location = new System.Drawing.Point(0, 0);
+            this.controlOldContentTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.controlOldContentTextBox.Multiline = true;
+            this.controlOldContentTextBox.Name = "controlOldContentTextBox";
+            this.controlOldContentTextBox.ReadOnly = true;
+            this.controlOldContentTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.controlOldContentTextBox.Size = new System.Drawing.Size(1008, 530);
+            this.controlOldContentTextBox.TabIndex = 0;
+            // 
+            // controlCurrentContentTextBox
+            // 
+            this.controlCurrentContentTextBox.AcceptsReturn = true;
+            this.controlCurrentContentTextBox.AcceptsTab = true;
+            this.controlCurrentContentTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlCurrentContentTextBox.HideSelection = false;
+            this.controlCurrentContentTextBox.Location = new System.Drawing.Point(0, 0);
+            this.controlCurrentContentTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.controlCurrentContentTextBox.Multiline = true;
+            this.controlCurrentContentTextBox.Name = "controlCurrentContentTextBox";
+            this.controlCurrentContentTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.controlCurrentContentTextBox.Size = new System.Drawing.Size(1008, 201);
+            this.controlCurrentContentTextBox.TabIndex = 1;
+            this.controlCurrentContentTextBox.TextChanged += new System.EventHandler(this.controlCurrentContentTextBox_TextChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.controlOldContentTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.controlCurrentContentTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(1008, 737);
+            this.splitContainer1.SplitterDistance = 530;
+            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // controlStampTimer
+            // 
+            this.controlStampTimer.Interval = 1000;
+            this.controlStampTimer.Tick += new System.EventHandler(this.controlStampTimer_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 737);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Main";
+            this.Text = "Simple Stream Journal";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox controlOldContentTextBox;
+        private System.Windows.Forms.TextBox controlCurrentContentTextBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Timer controlStampTimer;
+    }
+}
+
---
+++ Simple Stream Journal/Main.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace Simple_Stream_Journal
+{
+    public partial class Main : Form {
+        public Main() {
+            InitializeComponent();
+
+        }
+
+        private void Main_Shown(object sender, EventArgs e) {
+            timeSinceLastChange = 0;
+            needsSaved = false;
+
+            startDateTime = new DateTime();
+            hasStartDateTime = false;
+
+            Open();
+            NameTitle();
+        }
+
+        private int timeSinceLastChange;
+        private bool needsSaved;
+
+        private DateTime startDateTime;
+        private bool hasStartDateTime;
+
+        private void Save() {
+            var Filename = "Journal.txt";
+            var Content =   controlOldContentTextBox.Text +
+                            Environment.NewLine + 
+                            "-Start-" + startDateTime.ToString("---yyyy-MM-dd-HH:mm:ss-") +
+                            Environment.NewLine +
+                            controlCurrentContentTextBox.Text +
+                            Environment.NewLine +
+                            "-Finish-" + DateTime.Now.AddSeconds(-7).ToString("---yyyy-MM-dd-HH:mm:ss---") +
+                            Environment.NewLine + 
+                            "==============================================";
+
+            File.WriteAllText(Filename, Content);
+
+            Open();
+
+            controlCurrentContentTextBox.Text = "";
+
+            needsSaved = false;
+            hasStartDateTime = false;
+        }
+
+        private void Open() {
+            var Filename = "Journal.txt";
+
+            if (!File.Exists(Filename)) {
+                File.WriteAllText(Filename, "");
+            }
+
+            using (var FileStream = new FileStream(Filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
+                using (var StreamReader = new StreamReader(FileStream, Encoding.ASCII, false)) {
+                    var text = StreamReader.ReadToEnd();
+                    controlOldContentTextBox.Text = text;
+                    controlOldContentTextBox.SelectionStart = controlOldContentTextBox.Text.Length;
+                    controlOldContentTextBox.ScrollToCaret();
+                    controlOldContentTextBox.SelectionStart = 0;
+                    controlOldContentTextBox.SelectionLength = 0;
+                }
+            }
+        }
+
+        private void NameTitle() {
+            var folderName = Directory.GetCurrentDirectory().Split(Path.DirectorySeparatorChar).Last();
+            Text = "Stream Journal - " + folderName;
+        }
+
+        private void controlStampTimer_Tick(object sender, EventArgs e) {
+            timeSinceLastChange++;
+
+            if (timeSinceLastChange > 6) {
+                Save();
+                controlStampTimer.Enabled = false;
+            }
+        }
+
+        private void controlCurrentContentTextBox_TextChanged(object sender, EventArgs e) {
+            timeSinceLastChange = 0;
+            needsSaved = true;
+            controlStampTimer.Enabled = true;
+            if(!hasStartDateTime) {
+                hasStartDateTime = true;
+                startDateTime = DateTime.Now;
+            }
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e) {
+            e.Cancel = needsSaved;
+        }
+
+    }
+}
